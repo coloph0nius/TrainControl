324f331add4c3423d54e79f15127fe609097ba3b
Arff <arffbenj@gmail.com>
2017-12-25 14:07:32 +0100

    First commit

---
+++ ControlServer/src/ControlServer/parts/ControlServer.java
@@ -0,0 +1,42 @@
+package ControlServer.parts;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class ControlServer 
+{
+    private DatagramSocket socket;
+    private boolean running;
+    private static final int SERVERPORT = 4711;
+    private byte[] buf = new byte[1024];
+    
+    public ControlServer() throws SocketException
+    {
+        socket = new DatagramSocket(SERVERPORT);
+        System.out.println("server started on port "+ SERVERPORT + "...");
+    }
+    
+    public void run() throws IOException
+    {
+        running = true;
+        while (running)
+        {
+            DatagramPacket packet = new DatagramPacket (buf, buf.length);
+            socket.receive(packet);
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            String received = new String (packet.getData(), 0, packet.getLength());
+            System.out.printf("Received from %s, Port: %d. Message: %s\n", address.toString(), port, received);
+            
+            if (received.equals("end"))
+            {
+                running = false;
+            }
+        }
+        socket.close();
+        
+    }
+}
---
+++ ControlServer/src/ControlServer/parts/ServerLauncher.java
@@ -0,0 +1,13 @@
+package ControlServer.parts;
+
+public class ServerLauncher 
+{
+
+	public static void main(String[] args) throws Exception
+	{
+	    ControlServer cServer = new ControlServer();
+	    cServer.run();
+	    
+	}
+
+}
