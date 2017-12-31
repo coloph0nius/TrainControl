ccd05e7d1239ec3587c86a621733520adccd2947
Arff <arffbenj@gmail.com>
2017-12-31 12:22:44 +0100

    Updated constant strings in RemotePacket

--- ControlServer/src/ControlServer/packets/RemotePacket.java
+++ ControlServer/src/ControlServer/packets/RemotePacket.java
@@ -2,14 +2,14 @@ package ControlServer.packets;
 
 public class RemotePacket
 {
-    private static final String LOCOMOTIVE_PACKET ="remote-packet";
+    private static final String REMOTE_PACKET ="remote-packet";
     private static final String REQUEST_LIST = "request-list";
     private static final String SET_LOCO = "set-loco";
     
     public static void processRemoteRequest(String packetcontent[])
     {
         int length = packetcontent.length;
-        if (length == 2 && packetcontent[1].equals(REQUEST_LIST))
+        if (length == 2 && packetcontent[0].equals(REMOTE_PACKET) && packetcontent[1].equals(REQUEST_LIST))
         {
             //TODO send locoMap
             System.out.println("send locoMap");
